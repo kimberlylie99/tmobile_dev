d5b2f47b4e391fb4fe71c2c8ace64b51e3de0208
Lie <Kimberly.Lie2@T-Mobile.com>
2019-06-17 17:38:46 -0600

    fix data filter

--- mysite/tvision/views.py
+++ mysite/tvision/views.py
@@ -25,29 +25,47 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
 def home(request):
-    startDate = request.GET.get('pub_date')
-    endDate = request.GET.get('pub_date')
+        startDate = request.GET.get('startDate')
+        endDate = request.GET.get('endDate')
 
-    for pub_date in AutoPage.max_length:
-        if pub_date.AutoPage < startDate:
-            startDate = pub_date.AutoPage
-        elif pub_date.AutoPage > endDate:
-            endDate = pub_date.AutoPage
+        parsed_start_date = None
+        parsed_end_date = None
 
-    #Generating counts of the main objects
-    auto_name = AutoPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
-    release_name = ReleasePage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
-    bug_name = BugPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
+        if startDate and endDate:
+            errors = {}
+            try:
+                parsed_start_date = datetime.datetime.strptime(startDate, "%d/%m/%Y")
+            except ValueError as e:
+                errors["startDate"] = "Invalid date"
+            try:
+                parsed_end_date = datetime.datetime.strptime(endDate, "%d/%m/%Y")
+            except ValueError as e:
+                errors["endDate"] = "Invalid date"
 
-    context = {
-        'auto' : auto_name,
-        'release' : release_name,
-        'bugs' : bug_name,
-        'start_date' : startDate,
-        'end_date' : endDate,
-    }
+        if startDate and endDate:
+            data = AutoPage.objects.filter(date_range=[parsed_start_date,parsed_end_date])
+        else:
+            data = AutoPage.objects.none()
 
-    return render(request, "home.html", context)
+        #Generating counts of the main objects
+        #auto_name = AutoPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
+        #release_name = ReleasePage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
+        #bug_name = BugPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
+
+        auto_name = AutoPage.objects.all()
+        release_name = ReleasePage.objects.all()
+        bug_name = BugPage.objects.all()
+
+        context = {
+            #'auto' : auto_name,
+            'release' : release_name,
+            'bugs' : bug_name,
+            'start_date' : startDate,
+            'end_date' : endDate,
+            'data' : data,
+        }
+
+        return render(request, "home.html", context)
 
 def upload_pic(request):
     album = get_object_or_404(AutoPage, pk=1)
