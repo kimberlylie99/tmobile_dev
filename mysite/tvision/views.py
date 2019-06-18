cbb6bbe614e4f34b9aa2f418870864524e9d3870
Lie <Kimberly.Lie2@T-Mobile.com>
2019-06-18 15:18:29 -0600

    tried to fix date filter

--- mysite/tvision/views.py
+++ mysite/tvision/views.py
@@ -28,41 +28,45 @@ def home(request):
         startDate = request.GET.get('startDate')
         endDate = request.GET.get('endDate')
 
-        parsed_start_date = None
-        parsed_end_date = None
+#        parsed_start_date = None
+#        parsed_end_date = None
+#
+#        if startDate and endDate:
+#            errors = {}
+#            try:
+#                parsed_start_date = datetime.datetime.strptime(startDate, "%d/%m/%Y")
+#            except ValueError as e:
+#                errors["startDate"] = "Invalid date"
+#            try:
+#                parsed_end_date = datetime.datetime.strptime(endDate, "%d/%m/%Y")
+#            except ValueError as e:
+#                errors["endDate"] = "Invalid date"
 
-        if startDate and endDate:
-            errors = {}
-            try:
-                parsed_start_date = datetime.datetime.strptime(startDate, "%d/%m/%Y")
-            except ValueError as e:
-                errors["startDate"] = "Invalid date"
-            try:
-                parsed_end_date = datetime.datetime.strptime(endDate, "%d/%m/%Y")
-            except ValueError as e:
-                errors["endDate"] = "Invalid date"
 
+
+        #data = AutoPage.objects.filter(pub_date__range=[startDate,endDate])
         if startDate and endDate:
-            data = AutoPage.objects.filter(date_range=[parsed_start_date,parsed_end_date])
+            auto_data = AutoPage.objects.filter(pub_date__range=[startDate,endDate])
+            release_data = ReleasePage.objects.filter(pub_date__range=[startDate,endDate])
+            bug_data = BugPage.objects.filter(pub_date__range=[startDate,endDate])
         else:
-            data = AutoPage.objects.none()
+            auto_data = AutoPage.objects.all()
+            release_data = ReleasePage.objects.all()
+            bug_data = BugPage.objects.all()
 
         #Generating counts of the main objects
         #auto_name = AutoPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
         #release_name = ReleasePage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
         #bug_name = BugPage.objects.all().filter(startDate__gte=startDate, endDate__lte=endDate)
 
-        auto_name = AutoPage.objects.all()
-        release_name = ReleasePage.objects.all()
-        bug_name = BugPage.objects.all()
+#        auto_data = AutoPage.objects.all()
+#        release_data = ReleasePage.objects.all()
+#        bug_data = BugPage.objects.all()
 
         context = {
-            #'auto' : auto_name,
-            'release' : release_name,
-            'bugs' : bug_name,
-            'start_date' : startDate,
-            'end_date' : endDate,
-            'data' : data,
+            'auto' : auto_data,
+            'release' : release_data,
+            'bugs' : bug_data,
         }
 
         return render(request, "home.html", context)
