a91ac6b704f0a0c99ab0ed7b770a30a97cd10241
Lie <Kimberly.Lie2@T-Mobile.com>
2019-07-03 17:04:33 -0600

    add pie chart

--- mysite/tvision/views.py
+++ mysite/tvision/views.py
@@ -8,6 +8,9 @@ from django.shortcuts import get_object_or_404
 
 from django.views.generic import TemplateView
 
+# for pie chart stuff
+from pylab import *
+
 def home(request):
         startDate = request.GET.get('start_date')
         endDate = request.GET.get('end_date')
@@ -38,7 +41,23 @@ def upload_pic(request):
 # adding pie chart
 # https://github.com/agiliq/django-graphos
 # https://github.com/sainipray/neerajbyte
+#def piechart(request):
+#    """piechart stuff"""
+#    xdata = ["passed","blocked","retest","rollback-passed","rollback-failed","conditional","failed"]
+#    ydata = [75,0,0,0,0,10,15]
+
 def piechart(request):
-    """piechart stuff"""
-    xdata = ["passed","blocked","retest","rollback-passed","rollback-failed","conditional","failed"]
-    ydata = [75,0,0,0,0,10,15]
+#    figure(1,figsize(6,6))
+#    ax = axes([0.1,0.1,0.8,0.8])
+
+    labels = 'Frogs','Hogs','Dogs','Logs'
+    sizes = [15,30,45,10]
+    explode = (0,0.05,0,0)
+
+    fig1,ax1 = plt.subplots()
+    ax1.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=True,startangle=90,output_type='div')
+
+    ax1.axis('equal')
+#    title('Raining Hogs and Dogs',bbox={'facecolor':'0.8','pad':5})
+
+    plt.show()
